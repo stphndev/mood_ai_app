db83014cdad062f33f789b9e98f7c2805593be04
stphndev <133531810+stphndev@users.noreply.github.com>
2023-10-07 10:51:33 +0000

    App setup and Auth

---
+++ app/new-user/page.tsx
@@ -0,0 +1,5 @@
+const NewUser = () => {
+    return <div>Hello</div>
+  }
+  
+  export default NewUser
---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function SignUpPage() {
+  return (
+    <SignUp
+        redirectUrl="/new-user"
+        afterSignUpUrl="/new-user"
+    />
+  )
